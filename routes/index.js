a26f713b78b7772ce378fc0b8b5ae97de06177d9
GabrielGVCB <gabriel.vinicius06.gb@gmail.com>
2025-10-22 21:45:02 -0300

    feat: Adicionando post e form botao

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Sistema de Biblioteca' });
+});
+
+router.get('/login', function (req, res, next) {
+  res.send('Aqui vai aparecer o form de login - aguarde...');
+});
+
+router.get('/listagem', function (req, res, next) {
+  res.send('Aqui vai aparecer a lista de livros');
 });
 
 module.exports = router;
